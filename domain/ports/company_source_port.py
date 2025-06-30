f2012fc2b41625f8b40ca7c3d00f55e72aafede2
Fausto Stangler (Gamer) <fausto.stangler@gmail.com>
2025-06-29 22:07:43 -0300

    adjustments

--- domain/ports/company_source_port.py
+++ domain/ports/company_source_port.py
@@ -4,8 +4,10 @@ from abc import ABC, abstractmethod
 from typing import Callable, List, Optional, Set
 
 from domain.dto.raw_company_dto import CompanyRawDTO
-
-from .metrics_collector_port import MetricsCollectorPort
+from domain.ports import (
+    ExecutionResultDTO,
+    MetricsCollectorPort,
+)
 
 
 class CompanySourcePort(ABC):
@@ -18,7 +20,7 @@ class CompanySourcePort(ABC):
         skip_codes: Optional[Set[str]] = None,
         save_callback: Optional[Callable[[List[CompanyRawDTO]], None]] = None,
         max_workers: int | None = None,
-    ) -> List[CompanyRawDTO]:
+    ) -> ExecutionResultDTO[CompanyRawDTO]:
         """Fetch all available companies."""
         raise NotImplementedError
 
