e12aabef1ec7e268417792b80f9572099ec8d118
Fausto Stangler (Gamer) <fausto.stangler@gmail.com>
2025-07-01 23:02:20 -0300

    statements adjustments

--- presentation/cli.py
+++ presentation/cli.py
@@ -55,8 +55,8 @@ class CLIController:
         self.logger.log("Start FLY CLI", level="info")
 
         # Run the company synchronization workflow.
-        self._run_company_sync()
-        self._run_nsd_sync()
+        # self._run_company_sync()
+        # self._run_nsd_sync()
         self._run_statement_sync()
 
         # Indicate the CLI finished executing.
